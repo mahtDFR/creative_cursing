a92f2390cd3b9872e95ff87fc30463fe613ffafd
maht <mahtDFR@users.noreply.github.com>
2020-09-11 18:37:37 +0100

    Add files via upload

--- to_file.py
+++ to_file.py
@@ -1,40 +1,30 @@
 import random
 
-# open list_a.txt
-with open ("list_a.txt") as list_a:
-    a = [line.rstrip() for line in list_a]
-    list_a.close()
-
-# open list_b.txt
-with open ("list_b.txt") as list_b:
-    b = [line.rstrip() for line in list_b]
-    list_b.close()
-
-creative_curses = []
 count = 0
+curses = []
 
-# generate ONE MILLION results and dump them in the creative_curses list
+list_a = open("list_a.txt", "r")
+a = list_a.readlines()
+list_a.close()
+a = [line.rstrip() for line in a]
+
+list_b = open("list_b.txt", "r")
+b = list_b.readlines()
+list_b.close()
+b = [line.rstrip() for line in b]
+
+curse = (random.choice(a) + " " + random.choice(b))
+print("Generating ONE MILLION results. Please be patient, you " + curse.lower() + ".")
+
 while count < 1000000:
-
-    # make random combination
     curse = (random.choice(a) + " " + random.choice(b))
-
-    #TODO A horrible string capitalization method, but it works with names in the second tuple. Needs Improvement.
-    curse = curse.split(" ")
-    curse1 = curse[0].capitalize()
-    curse2 = curse[1]
-    curse = curse1 + " " +  curse2
-
-    # add result to list
-    list.append(creative_curses, curse)
-
-    # iterate
+    tweet = random.choice([curse.capitalize(), curse.upper()]) + (random.choice(["!", ".", ""]))
+    list.append(curses, tweet)
     count+=1
 
-# When total amount is achieved, dump the list to a file
 else:
-    print("Writing ONE MILLION results to file 'creative_curses.txt'. Please be patient, you " + curse.lower() + ".")
+    print("Writing results to file 'creative_curses.txt'")
     with open("creative_curses.txt", "w") as f:
-        f.write("\n".join(creative_curses))
+        f.write("\n".join(curses))
         f.close()
     exit()
